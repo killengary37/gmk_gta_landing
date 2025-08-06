6a99f4f6c2d4d49bcc8d4cc7c97adf02d7b4d4b8
Gary Marc Killen <killengary37@gmail.com>
2025-08-06 05:36:10 -0700

    Clean up the boilerplate, add the assets and set up the dependencies

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import gsap from "gsap";
+import {ScrollTrigger} from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const App = () => {
+  return (
+    <main>
+      <div>
+        <h1 className="text-3xl text-indigo-600">Welcome to the world of GTA and Gsap</h1>
+      </div>
+    </main>
+  )
+}
+export default App
