f0adce328a4d4af7237bb8174a41bcc66292a635
Gary Marc Killen <killengary37@gmail.com>
2025-08-19 17:03:09 -0700

    second video

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./sections/Hero.jsx";
 import NavBar from "./sections/NavBar.jsx";
 import FirstVideo from "./sections/FirstVideo.jsx";
 import Jason from "./sections/Jason.jsx";
+import SecondVideo from "./sections/SecondVideo.jsx";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -14,6 +15,7 @@ const App = () => {
      <Hero />
      <FirstVideo />
      <Jason />
+     <SecondVideo/>
     </main>
   )
 }
