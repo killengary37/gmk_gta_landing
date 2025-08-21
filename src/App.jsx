d3bdc7dd6495a391ce9c9a4c48148adc28de136a
Gary Marc Killen <killengary37@gmail.com>
2025-08-20 21:34:42 -0700

    postcard

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import FirstVideo from "./sections/FirstVideo.jsx";
 import Jason from "./sections/Jason.jsx";
 import SecondVideo from "./sections/SecondVideo.jsx";
 import Lucia from "./sections/Lucia.jsx";
+import PostCard from "./sections/PostCard.jsx";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -14,10 +15,14 @@ const App = () => {
     <main>
      <NavBar/>
      <Hero />
+
      <FirstVideo />
      <Jason />
+
      <SecondVideo/>
      <Lucia/>
+
+     <PostCard/>
     </main>
   )
 }
