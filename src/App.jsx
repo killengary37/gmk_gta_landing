9aef1e83faea6c7468008b772710c957035ed7be
Gary Marc Killen <killengary37@gmail.com>
2025-08-20 17:21:47 -0700

    lucia

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import NavBar from "./sections/NavBar.jsx";
 import FirstVideo from "./sections/FirstVideo.jsx";
 import Jason from "./sections/Jason.jsx";
 import SecondVideo from "./sections/SecondVideo.jsx";
+import Lucia from "./sections/Lucia.jsx";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -16,6 +17,7 @@ const App = () => {
      <FirstVideo />
      <Jason />
      <SecondVideo/>
+     <Lucia/>
     </main>
   )
 }
